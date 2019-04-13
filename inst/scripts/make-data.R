f48a8cd8b008f41ab3a89d474338fc7570267426
ericfournier2 <ericfournier2@yahoo.ca>
2019-04-12 23:41:16 -0400

    Update make-data, make-metadata.

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,9 +1,18 @@
+# prepare_ENCODEdb fetches the metadata tables from ENCODE and
+# store them as data.frame objects.
 tables = ENCODExplorerData::prepare_ENCODEdb("tables.rda")
+
+# export_ENCODEdb_matrix_lite stitches together the many tables 
+# by performing left_joins where appropriate.
 encode_dfs = export_ENCODEdb_matrix_lite(tables)
 
+# Write out encode_df_lite, which contains a limited,
+# curated set of metadata.
 encode_df_lite = encode_dfs$encode_df
-save(encode_df_lite, "encode_df_lite.rda")
+save(encode_df_lite, file="encode_df_lite.rda", compress="xz", compression_level=9)
 
+# Write out encode_df_full, which contains all processed metadata
+# columns.
 encode_df_full = do.call(cbind, encode_dfs)
-save(encode_df_lite, "encode_df_full.rda")
+save(encode_df_full, file="encode_df_full.rda", compress="xz", compression_level=9)
 
--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,5 +1,5 @@
 encode_lite_metadata_df = data.frame(
-    Title="ENCODE File Metadata (Light)",
+    Title="ENCODE File Metadata (Light, 2019-04-12 build)",
     Description="A data-frame containing a curated selection of metadata describing all files made available by the ENCODE project.",
     BiocVersion="3.9",
     Genome=NA,
@@ -11,13 +11,13 @@ encode_lite_metadata_df = data.frame(
     Coordinate_1_based=NA,
     DataProvider="ENCODE Project",
     Maintainer="Eric Fournier <Fournier.Eric.2@crchudequebec.ulaval.ca>",
-    RDataClass="data.frame",
+    RDataClass="data.table",
     DispatchClass="Rda",
-    RDataPath="", # TODO: fill this out once data have been uploaded to S3.
+    RDataPath="ENCODExplorerData/encode_df_lite.rda", # TODO: fill this out once data have been uploaded to S3.
     Tags="ENCODE")
     
 encode_full_metadata_df = data.frame(
-    Title="ENCODE File Metadata (Full)",
+    Title="ENCODE File Metadata (Full, 2019-04-12 build)",
     Description="A data-frame containing a large selection of metadata describing all files made available by the ENCODE project.",
     BiocVersion="3.9",
     Genome=NA,
@@ -29,9 +29,9 @@ encode_full_metadata_df = data.frame(
     Coordinate_1_based=NA,
     DataProvider="ENCODE Project",
     Maintainer="Eric Fournier <Fournier.Eric.2@crchudequebec.ulaval.ca>",
-    RDataClass="data.frame",
+    RDataClass="data.table",
     DispatchClass="Rda",
-    RDataPath="", # TODO: fill this out once data have been uploaded to S3.
+    RDataPath="ENCODExplorerData/encode_df_full.rda", # TODO: fill this out once data have been uploaded to S3.
     Tags="ENCODE")    
     
-write.csv(rbind(encode_lite_metadata_df, encode_full_metadata_df), file="inst/extdata/metadata.csv", row.names=FALSE)
+write.csv(rbind(encode_lite_metadata_df, encode_full_metadata_df), file="metadata.csv", row.names=FALSE)
